4227ad6bc27d3204ad3dc27aac7a709730883d7c
gnes <glauber.nespoli@gmail.com>
2020-06-26 23:04:50 +0200

    filepath fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * selects a random background image for the home 
  */
 function randomBG() {
-	var path = '../images/background/';
+	var path = './images/background/';
 	var bgArray = ['background1.jpeg', 'background2.jpeg', 'background3.jpeg'];
 	var selectedBG = bgArray[Math.floor(Math.random() * bgArray.length)];
 	document.getElementById('home').style.backgroundImage = 'url(' + path + selectedBG + ')';
